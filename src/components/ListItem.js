402560f9c38f7fc20e8e9504acf6de2fdcd15e1c
donatasz <d.zukauskas@superreal.de>
2017-03-22 15:29:49 +0200

    Refactor, use ES6 destructuring for props.

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -4,12 +4,13 @@ class ListItem extends Component {
 
     render() {
 
-        const props = this.props;
+        // ES6 destructuring
+        const { id, url, covers, name } = this.props.item;
 
         return (
-            <li className="project" key={props.item.id}>
-                <a className="project__link" href={props.item.url}>
-                    <img className="project__image" src={props.item.covers[404]} alt={props.item.name} title={props.item.name} />
+            <li className="project" key={id}>
+                <a className="project__link" href={url}>
+                    <img className="project__image" src={covers[404]} alt={name} title={name} />
                 </a>
             </li>
         );
